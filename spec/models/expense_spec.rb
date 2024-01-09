c376e2faa1b85cb251479f29aeea9ed39560c75d
Al-Amin <alaminkhanshakil@gmail.com>
2024-01-09 15:58:30 +0600

    Add routes

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'user 1') }
+
+  it 'name should be present' do
+    expense = Expense.new
+    expect()
 end
